9c6e962d79d1770937ef055f2d74d5920ce5a111
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2014-05-29 17:08:11 -0700

    Adding some finishing touches
    * adding timers to have phantom refresh itself
    * added timestamp to logger
    * start up phantom processes on startup

--- akabei.js
+++ akabei.js
@@ -1,18 +1,11 @@
 #!/usr/bin/env node
 
-var http = require('http');
+var akabeiServer = require('./lib/akabei_server.js');
 var PhantomSeleniumGrid = require('./lib/phantom_selenium_grid.js');
 
 var hostname = '0.0.0.0',
     port = 8000,
-    seleniumServerJar = './extbin/selenium-server-standalone-2.42.0.jar';
-
-/*
- http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
- }).listen(port, hostname);
-*/
+    seleniumServerJar = './bin/selenium-server-standalone-2.42.0.jar';
 
 var phantomSeleniumGridConfig = {
     'initialCount': 10
@@ -22,6 +15,9 @@ var seleniumGridConfig = {
     'seleniumServerJar': seleniumServerJar
 };
 
+var akabeiServerConfig = {
+};
+
 var grid = new PhantomSeleniumGrid(
     phantomSeleniumGridConfig,
     seleniumGridConfig
@@ -29,11 +25,8 @@ var grid = new PhantomSeleniumGrid(
 
 grid.start();
 
-setTimeout(function() {
-    console.log('hello!');
-}, 5000);
-// sg.stop();
 
-
-
-// console.log('Started server at ' + hostname + ':' + port);
+var app = akabeiServer(akabeiServerConfig, grid);
+app.listen(port, function () {
+    console.log('Started server at ' + hostname + ':' + port);
+});
