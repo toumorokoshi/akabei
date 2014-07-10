566603e46f0ef75bd4f40b541b1aa09f8636140c
yusuket <yusuket@zillow.com>
2014-07-10 13:40:54 -0700

    adding logging to a file

--- akabei.js
+++ akabei.js
@@ -1,20 +1,33 @@
 #!/usr/bin/env node
 
 var doc = "\
-akabei, a phantomjs cluster manager      \n\
-Usage:                                   \n\
-  akabei [<config_file> -v]              \n\
-  akabei -h | --help                     \n\
+akabei, a phantomjs cluster manager               \n\
+Usage:                                            \n\
+  akabei [-v -l <log_file>] [<config_file>]       \n\
+  akabei -h | --help                              \n\
+                                                  \n\
+Options:                                          \n\
+  -v, --verbose  will output debug info           \n\
+  -l, --log      path to a file to log output to  \n\
 ";
 
 var docopt = require('docopt');
 var logger = require('./lib/logger');
+var winston = require('winston');
 
 var args = docopt.docopt(doc),
     config_file = args['<config_file>'] || './config.js',
     loggingLevel = args['-v'] ? 'debug' : 'info';
 
-logger.transports.console.level = 'debug';
+logger.transports.console.level = loggingLevel;
+
+console.log(args);
+if (args['--log']) {
+    logger.add(winston.transports.File, {
+        filename: args['<log_file>'],
+        level: loggingLevel
+    });
+}
 
 console.log('loading config file from ' + config_file + '...');
 var config = require(config_file);
